9397733b1acd52241f583521094f70ac5993cd97
Youssef Ketata <yusufketata5@gmail.com>
2023-03-10 11:07:40 +0100

    Offer.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -167,32 +167,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __toString(): string
     {
-        // TODO: Implement __toString() method.
         return $this->email;
-    }
-
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(?\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
     }
 
 }
